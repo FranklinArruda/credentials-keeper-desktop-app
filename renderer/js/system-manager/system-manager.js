e0d12928d1d14674b16a9bbfd68c70ea34624e4c
Franklin Arruda <franklin.arrudaa@gmail.com>
2024-01-07 16:15:57 +0000

    System manager Semantic finished

---
+++ renderer/js/system-manager/system-manager.js
@@ -0,0 +1,25 @@
+
+    //const credentialsTitle = document.querySelector('.credentials-title');
+    const credentialsTitle = document.querySelector('.bottom-line');
+
+    function setActive(element) {
+        credentialsTitle.classList.remove('add');
+      //  phoneTitle.classList.remove('add');
+        element.classList.add('add');
+    }
+
+    // Set credentialsTitle as active by default
+    setActive(credentialsTitle);
+    
+    // phoneTitle.addEventListener('click', function () {
+    //     setActive(phoneTitle);
+    // });
+
+    // credentialsTitle.addEventListener('click', function () {
+    //     setActive(credentialsTitle);
+    // });
+
+    
+
+    
+

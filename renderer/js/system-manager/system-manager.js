5b2fbac885e65f46e75c1b9d4b1d9882d3d99aa3
FranklinArruda <franklin.arrudaa@gmail.com>
2024-01-14 16:32:36 +0000

    Table style finished + all  UI

--- renderer/js/system-manager/system-manager.js
+++ renderer/js/system-manager/system-manager.js
@@ -41,12 +41,6 @@ expandableElements.forEach(expandableElement => {
 
 
 
-
-
-
-
-       
-
        const credentialsMenu = document.querySelector('.credentials-title');
        const phoneMenu = document.querySelector('.phone-title');
        
@@ -78,9 +72,6 @@ expandableElements.forEach(expandableElement => {
        }
 
 
-    
-
-
   // testing end session  button from system
   const endSession = document.querySelector('.end-session');
   endSession.addEventListener('click', () => {
@@ -92,6 +83,186 @@ expandableElements.forEach(expandableElement => {
 
     
 
+////////////////////////////////    TABLE   credentials system  ////////////////////////////////////////////////////
+
+
+
+// ADD DATA TO TABLE (credentials systems)
+function addCredentialsData(event) { 
+  // Get input values 
+  let subject = 
+    document.getElementById("subject").value; 
+  let userName = 
+    document.getElementById("username").value; 
+  let password = 
+    document.getElementById("password").value; 
+  
+  // Get the table and insert a new row at the end 
+  let table = document.getElementById("outputTableCredentials"); 
+  let newRow = table.insertRow(table.rows.length); 
+  
+  // Insert data into cells of the new row 
+  newRow.insertCell(0).innerHTML = subject; 
+  newRow.insertCell(1).innerHTML = userName; 
+  newRow.insertCell(2).innerHTML = password; 
+  newRow.insertCell(3).innerHTML = 
+    '<button class="button action" onclick="editCredentialsData(this)">Edit</button>'+ 
+    '<button class="button action" onclick="deleteCredentialsData(this)">Delete</button>'; 
+  
+  // Clear input fields 
+  clearInputsCredentials(); 
+} 
+
+
+const buttonAddCredentials = document.querySelector(".add-button.credentials");
+buttonAddCredentials.addEventListener("click", function(event) {
+ 
+  // Prevent the default form submission
+  event.preventDefault();
+  addCredentialsData(event);
+});
+
+
+// EDIT TABLE
+function editCredentialsData(button) { 
+  
+  // Get the parent row of the clicked button 
+  let row = button.parentNode.parentNode; 
+  
+  // Get the cells within the row 
+  let nameCell = row.cells[0]; 
+  let emailCell = row.cells[1]; 
+  let mobileCell = row.cells[2]; 
+  
+  // Prompt the user to enter updated values 
+  let nameInput = 
+    prompt("Enter the updated name:", 
+      nameCell.innerHTML); 
+  let emailInput = 
+    prompt("Enter the updated email:", 
+      emailCell.innerHTML); 
+  let numberInput = 
+    prompt("Enter the updated mobile details:", 
+      mobileCell.innerHTML 
+    ); 
+  
+  
+  // Update the cell contents with the new values 
+  nameCell.innerHTML = nameInput; 
+  emailCell.innerHTML = emailInput; 
+  mobileCell.innerHTML = numberInput; 
+
+} 
+
+
+
+
+function deleteCredentialsData(button) { 
+  
+  // Get the parent row of the clicked button 
+  let row = button.parentNode.parentNode; 
+
+  // Remove the row from the table 
+  row.parentNode.removeChild(row); 
+} 
+
+
+function clearInputsCredentials() { 
+  // Clear input fields 
+  document.getElementById("subject").value = ""; 
+  document.getElementById("username").value = ""; 
+  document.getElementById("password").value = ""; 
+} 
+
+
+  
+
+////////////////////////////////    TABLE   phone keeper  ////////////////////////////////////////////////////
+
+
+
+// ADD DATA TO TABLE (phone keeper)
+function addPhoneData(event) { 
+  // Get input values 
+  let subject = 
+    document.getElementById("name").value; 
+  let userName = 
+    document.getElementById("phone-number").value; 
+  
+  
+  // Get the table and insert a new row at the end 
+  let table = document.getElementById("outputTablePhone"); 
+  let newRow = table.insertRow(table.rows.length); 
+  
+  // Insert data into cells of the new row 
+  newRow.insertCell(0).innerHTML = subject; 
+  newRow.insertCell(1).innerHTML = userName;  
+  newRow.insertCell(2).innerHTML = 
+    '<button class="button action" onclick="editDataPhone(this)">Edit</button>'+ 
+    '<button class="button action" onclick="deleteDataPhone(this)">Delete</button>'; 
+  
+  // Clear input fields 
+  clearInputsPhone(); 
+} 
+
+
+const buttonAddPhone = document.querySelector(".add-button.phone");
+buttonAddPhone.addEventListener("click", function(event) {
+ 
+  // Prevent the default form submission
+  event.preventDefault();
+  addPhoneData(event);
+});
+
+
+// EDIT TABLE
+function editDataPhone(button) { 
+  
+  // Get the parent row of the clicked button 
+  let row = button.parentNode.parentNode; 
+  
+  // Get the cells within the row 
+  let name = row.cells[0]; 
+  let phonenumber = row.cells[1]; 
+ 
+  
+  
+  // Prompt the user to enter updated values 
+  let nameInput = 
+    prompt("Enter the updated name:", 
+    name.innerHTML);
+
+  let phoneNumberInput = 
+    prompt("Enter the updated phone number:", 
+    phonenumber.innerHTML); 
+
+ 
+  
+  // Update the cell contents with the new values 
+  name.innerHTML = nameInput; 
+  phonenumber.innerHTML = phoneNumberInput; 
+  
+} 
+
+
+
+
+function deleteDataPhone(button) { 
+  
+  // Get the parent row of the clicked button 
+  let row = button.parentNode.parentNode; 
+
+  // Remove the row from the table 
+  row.parentNode.removeChild(row); 
+} 
+
+
+function clearInputsPhone() { 
+  // Clear input fields 
+  document.getElementById("name").value = ""; 
+  document.getElementById("phone-number").value = ""; 
+ 
+} 
 
 
 
@@ -103,16 +274,8 @@ expandableElements.forEach(expandableElement => {
 
 
 
-    
-//     function myFunction() {
-//         // Get the elements corresponding to the phone and credentials systems
-// const phoneSystem = document.querySelector('.phone-system');
-// const credentialsSystem = document.querySelector('.credentials-system');
 
-//        var x = document.getElementById("myDIV");
-//       if (x.style.display === "none") {
-//         x.style.display = "block";
-//       } else {
-//         x.style.display = "none";
-//       }
-//     }
+
+
+
+
